52b33f9b3fb99864141017fb1de3ee2d7426085f
JohnFreeman <hljohnfreeman@gmail.com>
2026-09-10 03:01:11 +0300

    update tools imports.cs
    Define the diff between editor related buttons/controlsheets and non editor one's, so it doesn't conflict ever

--- game/addons/tools/Code/Imports.cs
+++ game/addons/tools/Code/Imports.cs
@@ -1,6 +1,14 @@
 ﻿global using Editor;
 global using Sandbox;
 global using Sandbox.UI;
+
+// these exist in both Editor and Sandbox.UI, in tools code we mean the Editor ones
+global using Button = Editor.Button;
+global using Checkbox = Editor.Checkbox;
+global using Option = Editor.Option;
+global using ControlSheet = Editor.ControlSheet;
+global using ControlSheetGroup = Editor.ControlSheetGroup;
+global using ControlSheetRow = Editor.ControlSheetRow;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
